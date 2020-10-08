3628a169ca27ca3d54f8281ac3de7166627bfd58
ManuelCaballero <emanenxd@gmail.com>
2020-10-09 00:32:55 +0200

    c

---
+++ Sistema de gestión empresarial/01-HolaMundoWP/01-HolaMundoWF-Entities/clsPersona.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _01_HolaMundoWF_Entities
+{
+    public class clsPersona
+    {
+        #region Propiedades
+        public String nombre { get; set; }
+        #endregion
+
+        #region Constructores
+        #endregion
+    }
+}
---
+++ Sistema de gestión empresarial/01-HolaMundoWP/01-HolaMundoWP-UI/index.aspx.cs
@@ -0,0 +1,37 @@
+﻿using _01_HolaMundoWF_Entities;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _01_HolaMundoWP_UI
+{
+    public partial class index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSaludar_Click(object sender, EventArgs e)
+        {
+            clsPersona persona = new clsPersona();
+            String nombre = txbNombre.Text;
+
+            if (!String.IsNullOrEmpty(nombre))
+            {
+                lblSaludar.ForeColor = Color.Black;
+                persona.nombre = nombre;
+                lblSaludar.Text = "Hola "+persona.nombre;
+            }
+            else
+            {
+                lblSaludar.Text = "Escriba un nombre";
+                lblSaludar.ForeColor = Color.Red;
+            }
+        }
+    }
+}
