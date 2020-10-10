a8771670015b5c971ec5c0af24665c86e37b866a
ManuelCaballero <emanenxd@gmail.com>
2020-10-10 16:00:51 +0200

    Botones UWP

--- .Net/PruebaBotonexXAML/PruebaBotonexXAML/MainPage.xaml.cs
+++ .Net/PruebaBotonexXAML/PruebaBotonexXAML/MainPage.xaml.cs
@@ -29,7 +29,45 @@ namespace PruebaBotonexXAML
 
         private void btn2_Click(object sender, RoutedEventArgs e)
         {
+            if (stackPanel.FindName("btn3") == null)
+            {
+                Button btn3 = new Button();
+
+                btn3.Name = "btn3";
+                btn3.Content = "Boton 3";
+                btn3.HorizontalAlignment = HorizontalAlignment.Center;
+                btn3.VerticalAlignment = VerticalAlignment.Center;
+                btn3.Background = new SolidColorBrush(Windows.UI.Colors.Blue);
+                btn3.Width = 200;
+                btn3.Height = 70;
+                btn3.FontSize = 20;
+                btn3.FontFamily = new FontFamily("Verdana");
+                btn3.FontWeight = Windows.UI.Text.FontWeights.Bold;
+                btn3.BorderBrush = new SolidColorBrush(Windows.UI.Colors.Yellow);
+                btn3.Margin = new Thickness(30);
+                btn3.Click += btn3_Click;
+
+
+                stackPanel.Children.Add(btn3);
+            }
+        }
+
+        private void btn3_Click(object sender, RoutedEventArgs e)
+        {
+            if(btn1.IsEnabled)
+            {
+                btn1.IsEnabled = false;
+                btn1.Opacity = 0;
+            }
+            else
+            {
+                btn1.IsEnabled = true;
+                btn1.Opacity = 1;
+            }
 
         }
+
+
+
     }
 }
