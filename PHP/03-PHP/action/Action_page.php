bc133ff6c32a8eed2e4232374ab56bc7649b889b
ManuelCaballero <emanenxd@gmail.com>
2020-10-16 15:26:31 +0200

    php

--- PHP/03-PHP/action/Action_page.php
+++ PHP/03-PHP/action/Action_page.php
@@ -1,33 +1,67 @@
 <?php
 
-    include "../clase/Test.php";
-    include "../clase/Pregunta.php";
+//Importación de las clases utilizadas
+include "../clase/Test.php";
+include "../clase/Pregunta.php";
 
-    $preg1 = new Pregunta();
-    $preg2 = new Pregunta();
-    $preg3 = new Pregunta();
+$imagen = "";
 
-    $preg1->setRespuesta(3);
-    $preg2->setRespuesta(1);
-    $preg3->setRespuesta(1);
+//Se generan las preguntas
+$preg1 = new Pregunta();
+$preg2 = new Pregunta();
+$preg3 = new Pregunta();
 
-    $test = new Test();
+$preg1->setRespuesta(3);
+$preg2->setRespuesta(1);
+$preg3->setRespuesta(1);
 
-    $test->addPregunta(0,$preg1);
-    $test->addPregunta(1,$preg2);
-    $test->addPregunta(2,$preg3);
+$test = new Test();
 
-    for($i = 0; i <= 3; $i++)
-    {
-       if($test->getPregunta(0)->getRespuesta() == ($_POST[$i]))
-       {
-           $contadorAciertos++;
-       }
-    }
+//Se recuperan las respuestas del cliente
+$respuestasClientes = [];
 
-    echo $test->getPregunta(0)->getRespuesta()."<br>";
-    echo $test->getPregunta(1)->getRespuesta()."<br>";
-    echo $test->getPregunta(2)->getRespuesta()."<br>";
+$respuestasClientes[0] = (int)$_POST["preg1"];
+$respuestasClientes[1] = (int)$_POST["preg2"];
+$respuestasClientes[2] = (int)$_POST["preg3"];
 
+//Se guardan añaden nuevas preguntas
+$test->addPregunta(0,$preg1);
+$test->addPregunta(1,$preg2);
+$test->addPregunta(2,$preg3);
+
+//Se comprueban las respuestas
+
+$contadorAciertos = 0;
+
+
+for($i = 0; $i < 3; $i++)
+{
+    $respuesta = $test->getPregunta($i)->getRespuesta();
+
+    $respuestaCliente = $respuestasClientes[$i];
+
+   if($respuesta == $respuestaCliente)
+   {
+       $contadorAciertos++;
+   }
+}
+
+switch ($contadorAciertos)
+{
+    case 1:
+        $imagen = "../img/img1.jpg";
+        echo '<h1>Has tenido '.$contadorAciertos.' aciertos</h1><img src="'.$imagen.'" width: 30%; height: 100px;>';
+    break;
+    case 2:
+        $imagen = "../img/img2.jpg";
+        echo '<h1>Has tenido '.$contadorAciertos.' aciertos</h1><img src="'.$imagen.'" width: 30%; height: 100px;>';
+    break;
+    case 3:
+        $imagen = "../img/img3.jpg";
+        echo '<h1>Has tenido '.$contadorAciertos.' aciertos</h1><img src="'.$imagen.'" width: 30%; height: 100px;>';
+    break;
+    default:
+        echo "Me comes la pinga";
+}
 
 ?>
