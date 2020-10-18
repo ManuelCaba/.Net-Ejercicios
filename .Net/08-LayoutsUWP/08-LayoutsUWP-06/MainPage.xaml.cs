fb0d030834da2b68193970f34f0c1967426b97ea
ManuelCaballero <emanenxd@gmail.com>
2020-10-18 18:11:53 +0200

    DIN

--- .Net/08-LayoutsUWP/08-LayoutsUWP-06/MainPage.xaml.cs
+++ .Net/08-LayoutsUWP/08-LayoutsUWP-06/MainPage.xaml.cs
@@ -28,9 +28,122 @@ namespace _08_LayoutsUWP_06
             this.InitializeComponent();           
         }
 
-        public void nuevoFormulario()
+        /// <summary>
+        /// Borrará todos los campos del formulario
+        /// </summary>
+        public void nuevoFormulario(object sender, RoutedEventArgs e)
         {
+            txbNombre.Text = "";
+            txbApellidos.Text = "";
+            txbFecha.Text = "";
+        }
 
+        /// <summary>
+        /// Validará todos los campos del formulario
+        /// </summary>
+        public void guardarFormulario(object sender, RoutedEventArgs e)
+        {
+            if(validarCampoFecha() & validarCampoNombre() & validarCampoApellidos())
+            {
+                txkFormularioGuardado.Text = "Se ha guardado correctamente";
+            }
+            else
+            {
+                txkFormularioGuardado.Text = "";
+            }
+        }
+
+        /// <summary>
+        /// Valida si el campo de nombre esta vacío, en casa de estarlo
+        /// mandará un mensaje de error. Se mandará un boolean, true para
+        /// campo lleno y false para campo vacío.
+        /// </summary>
+        public bool validarCampoNombre()
+        {
+            bool campoLleno = true;
+
+            if(String.IsNullOrEmpty(txbNombre.Text))
+            {
+                txkErrorNombre.Text = "El nombre no puede estar vacío";
+                campoLleno = false;
+            }
+            else
+            {
+                txkErrorNombre.Text = "";
+            }
+
+            return campoLleno;
+        }
+
+        /// <summary>
+        /// Valida si el campo de fecha esta vacío, en casa de estarlo
+        /// mandará un mensaje de error. Se mandará un boolean, true para
+        /// campo lleno y false para campo vacío.
+        /// </summary>
+        public bool validarCampoFecha()
+        {
+            bool campoLleno = true;
+
+            if (String.IsNullOrEmpty(txbFecha.Text))
+            {
+                txkErrorFecha.Text = "La fecha no puede estar vacía";
+                campoLleno = false;
+            }
+            else
+            {
+                txkErrorFecha.Text = "";
+            }
+
+            return campoLleno;
+        }
+
+        /// <summary>
+        /// Valida si el campo de apellidos esta vacío, en casa de estarlo
+        /// mandará un mensaje de error. Se mandará un boolean, true para
+        /// campo lleno y false para campo vacío.
+        /// </summary>
+        public bool validarCampoApellidos()
+        {
+            bool campoLleno = true;
+
+            if (String.IsNullOrEmpty(txbApellidos.Text))
+            {
+                txkErrorApellidos.Text = "Los apellidos no pueden estar vacíos";
+                campoLleno = false;
+            }
+            else
+            {
+                txkErrorApellidos.Text = "";
+            }
+
+            return campoLleno;
+        }
+
+
+        /// <summary>
+        /// Abre un cuadro de diálogo para confirmar si quiere borrar el
+        /// formulario, en caso positivo se borrará
+        /// </summary>
+        public async void borrarFormulario(object sender, RoutedEventArgs e)
+        {
+            ContentDialog deleteFileDialog = new ContentDialog
+            {
+                Title = "Desea boorar el formulario?",
+                Content = "Si lo borras, no podras recuperarlo. Quieres borrarlo?",
+                PrimaryButtonText = "Borrar",
+                CloseButtonText = "Cancelar"
+            };
+
+            ContentDialogResult result = await deleteFileDialog.ShowAsync();
+
+            // Borrar el formulario si el usuario pulsa el botón primario.
+            /// En otro lugar, nada.
+            if (result == ContentDialogResult.Primary)
+            {
+                txbNombre.Text = "";
+                txbApellidos.Text = "";
+                txbFecha.Text = "";
+            }
         }
     }
 }
