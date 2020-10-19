ddac6cea2d4dab334a083f65e7666f1edbbde1c4
ManuelCaballero <emanenxd@gmail.com>
2020-10-19 18:00:09 +0200

    razor

---
+++ Sistema de gestión empresarial/02-HolaMundoASP.NET/02-HolaMundoASP.NET-UI/Controllers/OperacionesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _02_HolaMundoASP.NET_UI.Controllers
+{
+    public class OperacionesController : Controller
+    {
+        // GET: Operaciones
+        public ActionResult TablaMultiplicar()
+        {
+            return View();
+        }
+    }
+}
