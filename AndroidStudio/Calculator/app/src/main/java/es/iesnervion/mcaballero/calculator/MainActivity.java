7f578bd27ff6e65d041445fe74aedee704f693b0
ManuelCaba <49060865+ManuelCaba@users.noreply.github.com>
2020-10-23 09:18:29 +0200

    Android Studio

--- AndroidStudio/Calculator/app/src/main/java/es/iesnervion/mcaballero/calculator/MainActivity.java
+++ AndroidStudio/Calculator/app/src/main/java/es/iesnervion/mcaballero/calculator/MainActivity.java
@@ -3,12 +3,95 @@ package es.iesnervion.mcaballero.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    //Botones
+    private Button btnPunto;
+    private Button btn0;
+    private Button btnComa;
+    private Button btnIgual;
+    private Button btn1;
+    private Button btn2;
+    private Button btn3;
+    private Button btnSuma;
+    private Button btn4;
+    private Button btn5;
+    private Button btn6;
+    private Button btnResta;
+    private Button btn7;
+    private Button btn8;
+    private Button btn9;
+    private Button btnMultiplicacion;
+    private Button btnAC;
+    private Button btnDivision;
+
+    //TextViews Solucion
+    private TextView txvSolucion;
+    private TextView txvOperaciones;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Inicializar botones
+        btnPunto = findViewById(R.id.btnPunto);
+        btn0 = findViewById(R.id.btn0);
+        btnComa = findViewById(R.id.btnComa);
+        btnIgual = findViewById(R.id.btnIgual);
+        btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
+        btn3 = findViewById(R.id.btn3);
+        btnSuma = findViewById(R.id.btnSuma);
+        btn4 = findViewById(R.id.btn5);
+        btn6 = findViewById(R.id.btn6);
+        btnResta = findViewById(R.id.btnResta);
+        btn7 = findViewById(R.id.btn7);
+        btn8 = findViewById(R.id.btn8);
+        btn9 = findViewById(R.id.btn9);
+        btnMultiplicacion = findViewById(R.id.btnMultiplicacion);
+        btnAC = findViewById(R.id.btnAC);
+        btnDivision = findViewById(R.id.btnDivision);
+
+        //Inicializar TextViews
+        txvSolucion = findViewById(R.id.txvSolucion);
+        txvOperaciones = findViewById(R.id.txvOperaciones);
+
+        btnPunto.setOnClickListener(this);
+        btn0.setOnClickListener(this);
+        btnComa.setOnClickListener(this);
+        btnIgual.setOnClickListener(this);
+        btn1.setOnClickListener(this);
+        btn2.setOnClickListener(this);
+        btn3.setOnClickListener(this);
+        btnSuma.setOnClickListener(this);
+        btn4.setOnClickListener(this);
+        btn5.setOnClickListener(this);
+        btn6.setOnClickListener(this);
+        btnResta.setOnClickListener(this);
+        btn7.setOnClickListener(this);
+        btn8.setOnClickListener(this);
+        btn9.setOnClickListener(this);
+        btnMultiplicacion.setOnClickListener(this);
+        btnAC.setOnClickListener(this);
+        btnDivision.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        Button btnActual = findViewById(v.getId());
+        double resultado = 0;
+
+        if(btnActual.getText().toString().matches("1|2|3|4|5|6|7|8|9|0"))
+        {
+
+        }
+
+
     }
 }
