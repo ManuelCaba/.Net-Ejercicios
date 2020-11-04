ca762fd5ed609cc293a5a84355710c3cafbdd1a6
ManuelCaballero <emanenxd@gmail.com>
2020-11-04 16:03:21 +0100

    ca

--- .Net/10-Binding-01/10-Binding-02/Models/clsPersona.cs
+++ .Net/10-Binding-01/10-Binding-02/Models/clsPersona.cs
@@ -1,14 +1,48 @@
 ﻿using System;
+using System.ComponentModel;
 
 namespace _10_Binding_02.Models
 {
-    public class clsPersona
+    public class clsPersona : INotifyPropertyChanged
     {
+        #region Atributos
+        private String nombre;
+        private String apellidos;
+        private String nombreCompleto;
+        #endregion
+
         #region Propiedades
         public int ID { get; set; }
-        public String nombre { get; set; }
-        public String apellidos { get; set; }
+
+        public String Nombre 
+        { 
+            get { return nombre; }
+            set
+            { 
+                nombre = value;
+                NotifyPropertyChanged("NombreCompleto");
+            } 
+        }
+
+        public String Apellidos
+        {
+            get { return apellidos; }
+            set 
+            { 
+                apellidos = value;
+                NotifyPropertyChanged("NombreCompleto");
+            }
+        }
+
+        public String NombreCompleto
+        {
+            get { return nombre+" "+apellidos; }
+            set { NombreCompleto = value; }
+        }
+
         public int edad { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
         #endregion
 
         #region Constructores
@@ -27,6 +61,18 @@ namespace _10_Binding_02.Models
             this.apellidos = "Caballero";
             this.edad = 20;
         }
+        #endregion
+
+        #region INotifyPropertyChanged
+
+        private void NotifyPropertyChanged(String property)
+        {
+            if(PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+
         #endregion
     }
 }
---
+++ .Net/10-Binding-01/10-Binding-03/ViewModels/clsListados.cs
@@ -0,0 +1,28 @@
+﻿using _10_Binding_03.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_Binding_03.Models
+{
+    class clsListados
+    {
+        /*
+         * Función que devuelve un ObservableCollection de clsPersona ya inicializados
+         */
+        public ObservableCollection<clsPersona> listadoPersonas()
+        {
+            ObservableCollection<clsPersona> listadoPersonas = new ObservableCollection<clsPersona>();
+
+            listadoPersonas.Add(new clsPersona("Manuel", "Caballero", DateTime.Parse("14-12-2000"), "648778933", "Calle Tintilla, 6"));
+            listadoPersonas.Add(new clsPersona("Juan", "Quiros", DateTime.Parse("11-7-2000"), "634191623", "Calle Sagrado Corazon de Jesus, 11"));
+            listadoPersonas.Add(new clsPersona("Isidro", "Sanchez", DateTime.Parse("02-02-2000"), "632193729", "Calle Polvoraa, 4, 6A"));
+            listadoPersonas.Add(new clsPersona("Manuel", "Franco", DateTime.Parse("25-04-1996"), "674291832", "Avenida Poeta Manuel Benitez Carrasco, 10, 5B"));
+
+            return listadoPersonas;
+        }
+    }
+}
