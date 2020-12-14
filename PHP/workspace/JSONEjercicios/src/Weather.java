663e24dc0b438d03f9200172c7502c1ebdce1df6
ManuelCaballero <emanenxd@gmail.com>
2020-12-14 09:39:21 +0100

    Modificaciones

--- PHP/workspace/JSONEjercicios/src/Weather.java
+++ PHP/workspace/JSONEjercicios/src/Weather.java
@@ -15,23 +15,23 @@ public class Weather {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-	    String sURL = "https://api.openweathermap.org/data/2.5/onecall?lat=9.70383&lon=124.048286&exclude=hourly,current,minutely,alerts&appid=ae8e1449f1ba12886ccd97d9ae5698e8"; //just a string
+	    String sURL = "http://localhost:8080/APIRestTiendaOnline/index.php/Product/1"; //just a string
 
 	    // Connect to the URL using java's native library
 	    try {
-			JSONObject objetoPadre = new JSONObject(IOUtils.toString(new URL(sURL), Charset.forName("UTF-8")));
+			JSONArray arrayPadre = new JSONArray(sURL);
 			
-			System.out.println(objetoPadre.toString());
+			System.out.println(arrayPadre.toString());
 		} catch (JSONException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} catch (MalformedURLException e) {
+		} /*catch (MalformedURLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
+		}*/
 	    
 		
 		//JSONObject objetoPadre = new JSONObject("https://api.openweathermap.org/data/2.5/onecall?lat=9.70383&lon=124.048286&exclude=hourly,current,minutely,alerts&appid=ae8e1449f1ba12886ccd97d9ae5698e8");
