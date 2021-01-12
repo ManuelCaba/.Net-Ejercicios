0ad4f8ad72a4e47784db47ea9994b09dca85f4a3
ManuelCaballero <emanenxd@gmail.com>
2021-01-12 16:53:04 +0100

    UWP

---
+++ .Net/12-ResourcesYValueConverters/12-ResourcesYValueConverters-02/Converters/DateConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace _12_ResourcesYValueConverters_02.Converters
+{
+    class DateConverter:IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            DateTime dateValue = (DateTime) value;
+
+            String dateString = dateValue.ToString("dd/MM/yyyy");
+
+            return dateString;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            String dateString = value as String;
+
+            DateTime dateValue = DateTime.Parse(dateString);
+
+            return dateValue;
+        }
+    }
+}
---
+++ .Net/12-ResourcesYValueConverters/12-ResourcesYValueConverters-02/Models/clsPersona.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.ComponentModel;
+using System.Globalization;
+
+namespace _12_ResourcesYValueConverters_02.Models
+{
+    public class clsPersona
+    {
+        #region Propiedades
+
+        public String Nombre { get; set; }
+
+        public String Apellidos { get; set; }
+
+        public DateTime FechaNacimiento { get; set; }
+
+        public String Telefono { get; set; }
+
+        public String Direccion { get; set; }
+        #endregion
+
+        #region Constructores
+        public clsPersona(String nombre, String apellidos, DateTime fechaNacimiento, String telefono, String direccion)
+        {
+            Nombre = nombre;
+            Apellidos = apellidos;
+            FechaNacimiento = fechaNacimiento;
+            Telefono = telefono;
+            Direccion = direccion;
+        }
+
+        public clsPersona()
+        {
+            Nombre = "Manuel";
+            Apellidos = "Caballero Leiva";
+            FechaNacimiento = DateTime.Parse("14-12-2000");
+            Telefono = "648778933";
+            Direccion = "Calle Tintilla, 6";
+        }
+        #endregion
+    }
+}
