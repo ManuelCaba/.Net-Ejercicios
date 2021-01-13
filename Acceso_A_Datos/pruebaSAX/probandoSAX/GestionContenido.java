204ba83aa153ab3aab50d2cf178059c6f96d63c9
ManuelCaballero <emanenxd@gmail.com>
2021-01-13 17:52:22 +0100

    ADCDAT

--- Acceso_A_Datos/pruebaSAX/probandoSAX/GestionContenido.java
+++ Acceso_A_Datos/pruebaSAX/probandoSAX/GestionContenido.java
@@ -8,6 +8,8 @@ public class GestionContenido extends DefaultHandler {
 	private Jugador jugadorAnterior;
 	private Jugador jugadorActual;
 	private String etiquetaActual;
+	private Jugador jugadorGanador;
+	private String numeroPartida;
 
     public GestionContenido() {
         super();
@@ -21,14 +23,59 @@ public class GestionContenido extends DefaultHandler {
     @Override
     public void startElement(String uri, String nombre, String nombreC, Attributes att){
         etiquetaActual = nombreC;
+        
+        if(nombreC.equals("partida"))
+        {
+        	numeroPartida = att.getValue("id");
+        }
     }
     @Override
     public void endElement(String uri, String nombre, String nombreC){
     	
-    	switch(etiquetaActual)
+    	switch(nombreC)
     	{
     		case "jugador":
+    			
+    			double cantidadGanador;
+    			double cantidadActual = jugadorActual.getCantidadJugada();;
+    			double cantidadAnterior;
+    			
+    			if(jugadorAnterior != null)
+    			{
+    				
+    				if(jugadorGanador != null)
+    				{
+    					cantidadGanador = jugadorGanador.getCantidadJugada();
+    					cantidadAnterior = jugadorAnterior.getCantidadJugada();
+    					
+    					if(cantidadActual <= 7.5)
+    					{
+    						if(cantidadGanador < cantidadActual)
+    						{
+    							jugadorGanador = new Jugador(jugadorActual);
+    						}
+    					}
+    				}
+    				else if(cantidadActual <= 7.5)
+    				{
+    					jugadorGanador = new Jugador(jugadorActual);
+    				}
+    			}
+				else if(cantidadActual <= 7.5)
+				{
+					jugadorGanador = new Jugador(jugadorActual);
+				}
+    			
     			jugadorAnterior = new Jugador(jugadorActual);
+    		break;
+    		
+    		case "partida":
+    			
+    			System.out.println("Partida "+numeroPartida+" - Ganador/a: "+jugadorGanador.getNombre());
+    			
+    			jugadorGanador = null;
+    			jugadorAnterior = null;
+    			jugadorActual = null;
     		break;
     	}
     }
@@ -38,22 +85,33 @@ public class GestionContenido extends DefaultHandler {
     	
     	switch(etiquetaActual)
     	{
-    		case "jugador":
-    			jugadorActual = new Jugador(new String(ch, inicio, longitud));
+    		case "nombre":
+    			String nombre = new String(ch, inicio, longitud);
+    			
+    			if(!nombre.equals("\n"))
+    			{
+    				jugadorActual = new Jugador(new String(ch, inicio, longitud));
+    			}
     		break;
     		case "numero":
-    			String numero = new String(ch, inicio, longitud);
-    			double cantidad = 0;
-    			try
-    			{
-    				cantidad = Double.parseDouble(numero);
-    			}
-    			catch(NumberFormatException e)
-    			{
-    				cantidad = 0.5;
-    			}
     			
-    			jugadorActual.setCantidadJugada(jugadorActual.getCantidadJugada() + cantidad);
+    			String numero = new String(ch, inicio, longitud);
+    			
+    			if(!numero.equals("\n"))
+    			{
+        			double cantidad = 0;
+        			
+        			try
+        			{
+        					cantidad = Double.parseDouble(numero);
+        			}
+        			catch(NumberFormatException e)
+        			{
+        				cantidad = 0.5;
+        			}
+        			
+        			jugadorActual.setCantidadJugada(jugadorActual.getCantidadJugada() + cantidad);
+    			}
     			
 			break;
     		
