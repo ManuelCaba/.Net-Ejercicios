84e78f2372506635adc4f88a2e4eeee0839d9159
ManuelCaba <emanenxd@gmail.com>
2021-01-18 14:34:15 +0100

    JavaScript

---
+++ Sistema de gestión empresarial/PruebaJS/JS/JavaScriptFormulario.js
@@ -0,0 +1,18 @@
+﻿window.onload = inicializaEventos;
+
+function inicializaEventos() {
+
+    document.getElementById("btnSaludar").addEventListener("click", saludar, false);
+
+}
+
+function saludar() {
+
+    var nombre = document.getElementById("inpNombre").value;
+    var apellidos = document.getElementById("inpApellidos").value;
+
+    let persona = new clsPersona(nombre, apellidos);
+
+    alert("Hola " + persona.getNombre() + " " + persona.getApellidos());
+
+}
---
+++ Sistema de gestión empresarial/PruebaJS/JS/clsPersona.js
@@ -0,0 +1,23 @@
+﻿class clsPersona {
+
+    constructor(nombre, apellidos) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+    }
+
+    getNombre() {
+        return this.nombre;
+    }
+
+    setNombre(nombre) {
+        this.nombre = nombre;
+    }
+
+    getApellidos() {
+        return this.apellidos;
+    }
+
+    setApellidos(apellidos) {
+        this.apellidos = apellidos;
+    }
+}
