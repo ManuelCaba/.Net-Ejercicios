b9d74a9f042d8caacc493cd53357278595fca29a
ManuelCaba <emanenxd@gmail.com>
2021-02-28 15:29:08 +0100

    uwp

---
+++ .Net/ExamenUWP/ExamenUWPBL/Manejadoras/clsManejadoraPersonasBL.cs
@@ -0,0 +1,43 @@
+﻿using ExamenUWPDAL.Manejadoras;
+
+namespace _11_CRUDPersonasDepartamentos_BL.Manejadoras
+{
+    public class clsManejadoraPersonasBL
+    {
+        /// <summary>
+        /// Elimina una persona en la base de datos a partir de su ID
+        /// </summary>
+        /// <param name="IDPersona"></param>
+        /// <returns></returns>
+        public int eliminarPersonaBL(int IDPersona)
+        {
+            clsManejadoraPersonasDAL clsManejadoraPersonas = new clsManejadoraPersonasDAL();
+
+            return clsManejadoraPersonas.eliminarPersonaDAL(IDPersona);
+        }
+
+        /// <summary>
+        /// Edita una persona en la base de datos a partir de un objeto persona
+        /// </summary>
+        /// <param name="persona"></param>
+        /// <returns></returns>
+        public int editarPersonaBL(clsPersona persona)
+        {
+            clsManejadoraPersonasDAL clsManejadoraPersonas = new clsManejadoraPersonasDAL();
+
+            return clsManejadoraPersonas.editarPersonaDAL(persona);
+        }
+
+        /// <summary>
+        /// Crear una persona en la base de datos a partir de un objeto persona
+        /// </summary>
+        /// <param name="persona"></param>
+        /// <returns></returns>
+        public int crearPersonaBL(clsPersona persona)
+        {
+            clsManejadoraPersonasDAL clsManejadoraPersonas = new clsManejadoraPersonasDAL();
+
+            return clsManejadoraPersonas.crearPersonaDAL(persona);
+        }
+    }
+}
---
+++ .Net/ExamenUWP/ExamenUWPUI/ViewModels/Utilidades/clsVMBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenUWPUI.ViewModels.Utilidades
+{
+    public abstract class clsVMBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void NotifyPropertyChanged(String property = null)
+        {
+             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));   
+        }
+    }
+}
