cd9833d1288bf9b292fbf16eb31ef12b6745b42f
ManuelCaballero <emanenxd@gmail.com>
2021-03-09 21:49:43 +0100

    cs

---
+++ Sistema de gestión empresarial/07-MVCInicioUI/07-MVCInicioUI/Controllers/DefaultController.cs
@@ -0,0 +1,18 @@
+﻿using _07_MVCInicioUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _07_MVCInicioUI.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View(new clsListaPersonas().listaPersonas);
+        }
+    }
+}
